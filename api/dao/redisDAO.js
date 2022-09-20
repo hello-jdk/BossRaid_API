a84284ad219af67801984487633eb41f87f6f064
JDK <ehdrjf2062@gmail.com>
2022-09-21 02:32:03 +0900

    Feat: 랭킹리스트 유저생성DAO 작성

--- api/dao/redisDAO.js
+++ api/dao/redisDAO.js
@@ -2,6 +2,7 @@ const axios = require("axios").default;
 const { redis } = require("../../models");
 const URL = "https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json";
 
+//레이드상태 확인
 async function checkRaidStatus() {
   const canEnter = await redis.hGet(URL, "canEnter");
 
@@ -12,10 +13,12 @@ async function checkRaidStatus() {
   return canEnter;
 }
 
+//레이드에 입장한 유저조회
 async function getEnteredUserId() {
   return await redis.hGet(URL, "enteredUserId");
 }
 
+//레이드정보가 존재하는가
 async function checkRaidInfo(level) {
   const limitTimeExist = await redis.hExists(URL, "bossRaidLimitSeconds");
   const levelExist = await redis.hExists(URL, level);
@@ -27,6 +30,7 @@ async function checkRaidInfo(level) {
   }
 }
 
+//레이드정보를 생성
 async function createRaidInfo(level) {
   axios.get(URL).then((res) => {
     const bossDatum = res.data.bossRaids[0];
@@ -39,6 +43,7 @@ async function createRaidInfo(level) {
   });
 }
 
+//레이드정보를 조회
 async function getRaidInfo(level) {
   let raidInfo = {};
 
@@ -48,20 +53,34 @@ async function getRaidInfo(level) {
   return raidInfo;
 }
 
+//레이드정보 점수만 조회
+async function getScore() {
+  return await redis.hGet(URL, "score");
+}
+
+//레이드상태변경 (입장시)
 async function updateRaidSatusByEnter(userId, score) {
   await redis.hSet(URL, "canEnter", 0);
   await redis.hSet(URL, "enteredUserId", userId);
   await redis.hSet(URL, "score", score);
 }
 
+//레이드상태변경 (퇴장시)
 async function updateRaidSatusByEnd() {
   await redis.hSet(URL, "canEnter", 1);
   await redis.hDel(URL, "enteredUserId");
   await redis.hDel(URL, "score");
 }
 
-async function getScore() {
-  return await redis.hGet(URL, "score");
+//랭킹 생성
+async function createRank(userId) {
+  userId = String(userId); //삭제하면안됨
+  return await redis.ZADD("leaderboard", { value: userId, score: 0 });
+}
+
+//랭킹 전체 조회
+async function getTopRankList() {
+  return await redis.zRangeWithScores("leaderboard", 0, -1);
 }
 
 module.exports = {
@@ -73,4 +92,6 @@ module.exports = {
   updateRaidSatusByEnter,
   updateRaidSatusByEnd,
   getScore,
+  createRank,
+  getTopRankList,
 };
